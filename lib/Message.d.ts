f0344ee4d52e540c64f067a02d516a995fb12ab4
amatsegor <macegorarthur@gmail.com>
2017-05-30 19:11:36 +0300

    Restructiring

---
+++ lib/Message.d.ts
@@ -0,0 +1,26 @@
+import {TextMessageRouter} from "../src/routers/TextMessageRouter";
+import {AttachmentMessageRouter} from "../src/routers/AttachmentMessageRouter";
+import {UrlMessageRouter} from "../src/routers/UrlMessageRouter";
+import {QuickReplyRouter} from "../src/routers/QuickReplyMessageRouter";
+import {SenderActionMessageRouter} from "../src/routers/SenderActionMessageRouter";
+/**
+ * Created by amatsegor on 5/30/17.
+ */
+
+export declare class Message {
+    static textMessage(message: string, metadata?: string): TextMessageRouter;
+
+    static audioMessage(audioUrl: string): UrlMessageRouter;
+
+    static fileMessage(fileUrl: string): UrlMessageRouter;
+
+    static videoMessage(videoUrl: string): UrlMessageRouter;
+
+    static imageMessage(videoUrl: string): UrlMessageRouter;
+
+    static attachmentMessage(): AttachmentMessageRouter;
+
+    static quickReplyMessage(): QuickReplyRouter;
+
+    static senderActionMessage(): SenderActionMessageRouter;
+}
