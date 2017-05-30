c3361ca66619ab40636b25dcca9c022d105668ab
amatsegor <macegorarthur@gmail.com>
2017-05-30 20:09:50 +0300

    Try

---
+++ lib/models/FacebookMessage.d.ts
@@ -0,0 +1,10 @@
+import {Recipient} from "../../src/models/Recipient";
+import {AbstractMessage} from "../../src/message_builders/AbstractMessageBuilder";
+/**
+ * Created by amatsegor on 5/30/17.
+ */
+
+export declare class FacebookMessage {
+    recipient: Recipient;
+    message: AbstractMessage;
+}
